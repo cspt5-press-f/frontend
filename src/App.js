4558fa6717255fbb37146737ae3daac14d477077
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-02-26 18:23:55 -0800

    Added testing inputs to allow change of base url for testing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { login, register } from "./components/auth";
 function App() {
   const [loginDetails, setLoginDetails ] = useState( {username:" ", password: ""});
   const [registerDetails, setRegisterDetails ] = useState( {username:" ", password1: "", password2: ""});
+  const [baseUrl, setBaseUrl] = useState("https://lambda-mud-test.herokuapp.com");
   
   const loginChangeHandler = (e) => {
     console.log("login change", loginDetails);
@@ -19,23 +20,34 @@ function App() {
     setRegisterDetails(Object.assign({},registerDetails, {[e.target.name]: e.target.value}));
   }
 
-  const loginHandler = (data) => {
-
-    console.log("login data", data);
-    login(data);
+  const urlChangeHandler = (e) => {
+    console.log("Base url changed to ", baseUrl);
+    setBaseUrl(e.target.value);
   }
 
-  const registerHandler = (data) => {
+  const loginHandler = (baseUrl, data) => {
+
+    console.log("login data", data);
+    login(baseUrl, data);
+  }
+
+  const registerHandler = (baseUrl, data) => {
     console.log("register data", data);
     
-    register(data);
+    register(baseUrl, data);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <form>
+          <h1>Base URL</h1>
+          This will be the base url, and the login and registration will append "/api" to the requests.<br />
+          <input type="text" name="base-url" value={baseUrl} style={{width: "25vw"}}/>
+        </form>
+        <form>
           <h1>Login</h1>
+          {`${baseUrl}/api/login/`}<br />
           <label>
             Name:
             <input type="text" name="username" onChange={loginChangeHandler} value={loginDetails.username}/>
@@ -44,11 +56,12 @@ function App() {
             Password:
             <input type="password" name="password" onChange={loginChangeHandler} value={loginDetails.password}/>
           </label>
-          <button type="button" value="Submit" onClick={()=>loginHandler(loginDetails)}>Login</button>
+          <button type="button" value="Submit" onClick={()=>loginHandler(baseUrl, loginDetails)}>Login</button>
         </form>
         <br /><br />
         <form>
           <h1>Register</h1>
+          {`${baseUrl}/api/registration/`}<br />
           <label>
             Name:
             <input type="text" name="username" onChange={registerChangeHandler} value={registerDetails.username}/>
@@ -61,7 +74,7 @@ function App() {
             Confirm Password:
             <input type="password" name="password2" onChange={registerChangeHandler} value={registerDetails.password2}/>
           </label><br />
-          <button type="button" value="Submit" onClick={()=>registerHandler(registerDetails)}>Register</button>
+          <button type="button" value="Submit" onClick={()=>registerHandler(baseUrl, registerDetails)}>Register</button>
         </form>
       </header>
     </div>
